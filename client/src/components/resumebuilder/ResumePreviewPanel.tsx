9f7f7da6d86da4b55c682d3414b1e01de3be748e
Lalman <lalmanchaudhary2@gmail.com>
2026-05-22 09:08:53 +0530

    feat: add print CSS and print-area id for resume download

--- client/src/components/resumebuilder/ResumePreviewPanel.tsx
+++ client/src/components/resumebuilder/ResumePreviewPanel.tsx
@@ -1,32 +1,34 @@
-import React from "react";
+import React, { memo } from "react";
 import { motion } from "framer-motion";
 import ResumePreview from "./ResumePreview";
 import type { ResumeData } from "../../utils/types";
-
 
 interface ResumePreviewPanelProps {
   resumeData: ResumeData;
 }
 
-const ResumePreviewPanel: React.FC<ResumePreviewPanelProps> = ({
-  resumeData,
-}) => {
-  return (
-    <motion.aside
-      className="lg:col-span-7 max-lg:mt-6"
-      aria-label="Resume preview"
-      initial={{ opacity: 0, x: 30 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.4 }}
-    >
-      
-      <ResumePreview
-        data={resumeData}
-        template={resumeData.template}
-        accentColor={resumeData.accent_color}
-      />
-    </motion.aside>
-  );
-};
+const ResumePreviewPanel: React.FC<ResumePreviewPanelProps> = memo(
+  ({ resumeData }) => {
+    return (
+      <motion.aside
+        className="lg:col-span-7 max-lg:mt-6"
+        aria-label="Resume preview"
+        initial={{ opacity: 0, x: 30 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.4 }}
+      >
+        <div id="print-area">
+          <ResumePreview
+            data={resumeData}
+            template={resumeData.template}
+            accentColor={resumeData.accent_color}
+          />
+        </div>
+      </motion.aside>
+    );
+  },
+);
+
+ResumePreviewPanel.displayName = "ResumePreviewPanel";
 
 export default ResumePreviewPanel;
